be2689ff794e77757bf97926dc0a57e08cf870eb
Gaston Avila <avila.gas@gmail.com>
2016-01-07 13:49:21 -0300

    [finishes #111176774] Ocultar el selector de hojas de creacion de vista cuando en modo avanzado

--- workspace/media/workspace/scripts/createDataview/views/steps/SelectDataView.js
+++ workspace/media/workspace/scripts/createDataview/views/steps/SelectDataView.js
@@ -234,20 +234,28 @@ var SelectDataView = Backbone.View.extend({
         this.editArgumentsOverlayView.render();
     },
 
+    hideTablesView: function (shouldHide) {
+        this.$('.select-table-view').toggleClass('hidden', shouldHide);
+    },
+
     onChangeMode: function (model, value) {
         var currentlyEditingModel = model.get('currently-editing-model');
         if (value === 'headers') {
+            this.hideTablesView(true);
             this.selectionView.hide();
             this.headersOptionsView.show();
         } else if (value === 'add-filter') {
+            this.hideTablesView(true);
             this.disableSelection();
             this.selectionView.hide();
             this.attachFiltersView(currentlyEditingModel);
         } else if (value === 'set-formats') {
+            this.hideTablesView(true);
             this.disableSelection();
             this.selectionView.hide();
             this.attachFormatsView(currentlyEditingModel);
         } else {
+            this.hideTablesView(false);
             model.unset('currently-editing-model');
             this.enableSelection();
             this.destroyFiltersView();
