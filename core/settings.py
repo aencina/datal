43b8e9b75fdc9633e5fa97a28de75e72379ed8c7
Matias Saguir <mativs@gmail.com>
2016-01-13 18:17:04 -0300

    Paso a cualquier hosts allowed

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ PROJECT_PATH, FILENAME = os.path.split(os.path.abspath(os.path.dirname(__file__)
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['.junar.com']
+ALLOWED_HOSTS = ['*']
 
 ADMINS = (
     ('Webmaster', 'webmaster@junar.com'),
