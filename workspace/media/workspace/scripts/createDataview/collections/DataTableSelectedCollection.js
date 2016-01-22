0d8e76bd175ac96594706cb4e34829dcf395ca6c
dbleyenda <ignacio.sans@junar.com>
2016-01-21 22:23:16 -0300

    [workspace] duplicar data table en create Dataview para avilaton

---
+++ workspace/media/workspace/scripts/createDataview/collections/DataTableSelectedCollection.js
@@ -0,0 +1,47 @@
+var DataTableSelectedCollection = Backbone.Collection.extend({
+    model: DataTableSelectionModel,
+
+    setCache: function () {
+        this._cache = this.reduce(function (memo, m) {
+            memo[m.get('name')] = m.get('excelRange');
+            return memo;
+        }, {});
+        return this._cache;
+    },
+
+    revert: function () {
+        _.each(this._cache, function (value, key) {
+            var m = this.find(function (model) {
+                return model.get('name') === key;
+            }).set('excelRange', value);
+        }, this);
+    },
+
+    setMaxCols: function (maxCols) {
+        this.maxCols = maxCols;
+    },
+
+    setMaxRows: function (maxRows) {
+        this.maxRows = maxRows;
+    },
+
+    getItemsByMode: function (mode) {
+        return this.filter(function (model) {
+            return model.get('mode') === mode;
+        });
+    },
+
+    hasItemsByMode: function (mode) {
+        return this.getItemsByMode(mode).length !== 0;
+    },
+
+    removeItemsByMode: function (mode) {
+        return this.remove(this.getItemsByMode(mode));
+    },
+
+    filterByMode: function (mode) {
+        return this.filter(function (model) {
+            return model.get('mode') === mode;
+        });
+    }
+});
