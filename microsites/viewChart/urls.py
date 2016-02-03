65fe5efdfc93118d574c3de513fddd83547307f6
avdata99 <andres@data99.com.ar>
2016-02-03 17:52:20 -0300

    por compatibilidad

--- microsites/viewChart/urls.py
+++ microsites/viewChart/urls.py
@@ -4,4 +4,7 @@ from microsites.viewChart.views import *
 urlpatterns = patterns('',
     url(r'^(?P<id>\d+)/(?P<slug>[A-Za-z0-9\-]+)/$', 'microsites.viewChart.views.view', name='chart_manager.view'),
     url(r'^embed/(?P<guid>[A-Z0-9\-]+)/$', 'microsites.viewChart.views.embed', name='viewChart.embed'),
+    # se agrega por reverses traidos de Junar 1 aun sin aplicar
+    url(r'^(?P<id>\d+)-(?P<slug>[A-Za-z0-9\-]+).download$', 'microsites.viewChart.views.download', 
+        name='viewChart.download'),   
 )
