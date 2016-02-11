f0c5f767b0b89b440568cd25580257021b594f77
Matias Saguir <mativs@gmail.com>
2016-02-11 14:41:27 -0300

    Agrego el clone a la api y al lifecycle

--- core/lifecycle/visualizations.py
+++ core/lifecycle/visualizations.py
@@ -206,6 +206,11 @@ class VisualizationLifeCycleManager(AbstractLifeCycleManager):
         self.visualization_revision.clone(status)
         self._update_last_revisions()
 
+    def clone(self):
+        vzr = self.visualization_revision.clone(self.visualization_revision.status)
+        self._update_last_revisions()
+        return vzr
+
     def _remove_all(self):
         self.visualization.delete()
         self._log_activity(ActionStreams.DELETE)
