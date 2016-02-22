2bb14b08fafc4eca9fa3f8a263c03e61c21a5e39
Daniel Padula <daniel.padula@gmail.com>
2016-02-22 14:58:53 -0300

    Fix para varios problemas con la cuenta de Chileco

--- core/management/commands/post_migrate_guid_duplicated.py
+++ core/management/commands/post_migrate_guid_duplicated.py
@@ -1,24 +1,42 @@
-# Para zafar por un problema particular con ChileCompra
-import json
-account = 5476
-guids = []
+from django.core.management.base import BaseCommand
 
-f = open('/tmp/{}/dataset.json'.format(account), 'r')
-fixtures = json.load(f)
-f.close()
-new_fixtures = []
-for fixture in fixtures:
-    if fixture['fields']['guid'] not in guids:
-        guids.append(fixture['fields']['guid'])
-    else:
-        print "VIEJO GUID: {}".format(fixture['fields']['guid'])
-        partes = fixture['fields']['guid'].split('-')
-        partes[4] = str(int(partes[4]) + 1)
-        new_guid = '-'.join(partes)
-        print "NUEVO GUID: {}".format(new_guid)
-        guids.append(new_guid)
-        fixture['fields']['guid'] = new_guid
-    new_fixtures.append(fixture)
-f = open('/tmp/{}/dataset.json'.format(account), 'w')
-json.dump(new_fixtures, f)
-f.close()
+from optparse import make_option
+
+from core.models import (Account, Tag, TagDataset, TagDatastream, TagVisualization)
+from core.choices import StatusChoices
+import json
+from django.db.models import Q
+
+
+class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option('-a', '--account',
+            dest='account',
+            type="string",
+            help='Reindex resources'),
+        )
+
+    def handle(self, *args, **options):
+        if options['account']:
+            guids = []
+
+            f = open('/tmp/{}/dataset.json'.format(options['account']), 'r')
+            fixtures = json.load(f)
+            f.close()
+            new_fixtures = []
+            for fixture in fixtures:
+                if fixture['fields']['guid'] not in guids:
+                    guids.append(fixture['fields']['guid'])
+                else:
+                    print "VIEJO GUID: {}".format(fixture['fields']['guid'])
+                    partes = fixture['fields']['guid'].split('-')
+                    partes[4] = str(int(partes[4]) + 1)
+                    new_guid = '-'.join(partes)
+                    print "NUEVO GUID: {}".format(new_guid)
+                    guids.append(new_guid)
+                    fixture['fields']['guid'] = new_guid
+                new_fixtures.append(fixture)
+            f = open('/tmp/{}/dataset.json'.format(options['account']), 'w')
+            json.dump(new_fixtures, f)
+            f.close()
