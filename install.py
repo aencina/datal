2975506d6a057969edfe32eef10080db6947f95b
Daniel Padula <daniel.padula@gmail.com>
2016-04-25 14:45:44 -0300

    Instalamos salt

--- install.py
+++ install.py
@@ -1,1 +1,12 @@
-#! /bin/python
+#! /usr/bin/python
+
+from subprocess import call
+
+SALT_INSTALLER_COMMAND = "curl -L https://bootstrap.saltstack.com | sudo sh"
+
+
+def install_salt():
+    print("Instalando SaltStack / Installing SaltStack")
+    return call(SALT_INSTALLER_COMMAND, shell=True)
+
+install_salt()
