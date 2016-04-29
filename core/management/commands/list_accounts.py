893268fe5caae643fda402a1555c62eebd523c2c
ignacionf <ignacionf@gmail.com>
2016-04-29 14:31:30 -0300

    hay cuentas sin domain

--- core/management/commands/list_accounts.py
+++ core/management/commands/list_accounts.py
@@ -10,6 +10,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print "ID;Account Name;Status;Level;Domain;Api Domain"
         for account in Account.objects.all():
-            domain= Preference.objects.get(account=account, key="account.domain").value
-            api_domain= Preference.objects.get(account=account, key="account.api.domain").value
+            try:
+                domain= Preference.objects.get(account=account, key="account.domain").value
+                api_domain= Preference.objects.get(account=account, key="account.api.domain").value
+            except:
+                domain = "Sin Definir"
+                api_domain = "Sin Definir"
             print "%s;%s;%s;%s;%s;%s" % (account.id,account.name, account.get_status_display(), account.level, domain, api_domain)
