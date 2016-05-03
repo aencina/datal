2fafe29b048009faf5df8768cd735f0bb1820355
Daniel Padula <daniel.padula@gmail.com>
2016-05-03 16:52:24 -0300

    Configuramos y reiniciamos

--- install.py
+++ install.py
@@ -5,6 +5,7 @@ import sys
 from subprocess import call
 
 SALT_INSTALLER_COMMAND = "curl -L https://bootstrap.saltstack.com | sudo sh"
+SALT_RESTART_COMMAND = "service salt-minion restart"
 python_git_pkg = 'python-git'
 minion_file = '/etc/salt/minion'
 
@@ -12,6 +13,12 @@ minion_file = '/etc/salt/minion'
 def install_salt():
     print("Instalando SaltStack / Installing SaltStack")
     return call(SALT_INSTALLER_COMMAND, shell=True)
+
+
+def restart_salt():
+    print("Reiniciando SaltStack / Restarting SaltStack")
+    return call(SALT_RESTART_COMMAND, shell=True)
+
 
 def install_deps():
     print("Instalando dependencias / Installing deps")
@@ -34,8 +41,13 @@ def install_deps():
 def salt_configuration():
     print("Configurando SaltStack / Setting up SaltStack")
     minion = """
-    fileserver_backend:
-      - git
+fileserver_backend:
+  - git
+
+gitfs_base: master
+
+gitfs_remotes:
+  - https://github.com/Junar/datal-formula.git
     """
     f = open(minion_file, 'w')
     f.writelines(minion)
@@ -44,3 +56,4 @@ def salt_configuration():
 install_salt()
 install_deps()
 salt_configuration()
+restart_salt()
