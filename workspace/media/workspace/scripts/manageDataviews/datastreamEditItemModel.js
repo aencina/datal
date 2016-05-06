a6708846858f008e35e88ea2429ca0adbc92858f
dbleyenda <ignacio.sans@junar.com>
2016-05-06 13:12:48 -0300

    cambio de status en set Data de edit dataview

--- workspace/media/workspace/scripts/manageDataviews/datastreamEditItemModel.js
+++ workspace/media/workspace/scripts/manageDataviews/datastreamEditItemModel.js
@@ -63,7 +63,8 @@ var DatastreamEditItemModel = Backbone.Epoxy.Model.extend({
         data.title = $.trim( this.get('title') );
         data.description = $.trim( this.get('description') );
         data.category = $('#id_category option:selected').val();
-        data.status = $('#id_status option:selected').val();
+        //data.status = $('#id_status option:selected').val();
+        data.status = this.get('status');
         data.datastream_revision_id = this.get('datastream_revision_id');
 
         // Prepare Sources for Data
