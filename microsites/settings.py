ad7b0920d56a997537b93113eba054009dab55d5
ignacionf <ignacionf@gmail.com>
2016-06-08 10:52:51 -0300

    ordeno la config del cache

--- microsites/settings.py
+++ microsites/settings.py
@@ -70,14 +70,10 @@ except ImportError:
 
 # TODO Esto no deberia ir en salt?
 # Agregamos la config para usar cache por pagina
+CACHES['pages']={ 'BACKEND': 'django.core.cache.backends.dummy.DummyCache', }
 if not DEBUG:
     CACHES['pages']={
             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
             'LOCATION': '127.0.0.1:11211',
+            'KEY_FUNCTION': 'core.decorators.datal_make_key',
         }
-else:
-    # disable cache_page
-    CACHES['pages']={ 'BACKEND': 'django.core.cache.backends.dummy.DummyCache', }
-
-#queda deshabilitado hasta nuevo aviso
-CACHES['pages']={ 'BACKEND': 'django.core.cache.backends.dummy.DummyCache', }
